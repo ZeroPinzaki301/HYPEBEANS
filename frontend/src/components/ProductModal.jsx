4d3d6553ba1e33e8131d1410d7e79d4b123c85f4
Zeraijin301 <138296747+ZeroPinzaki301@users.noreply.github.com>
2025-04-30 18:00:22 +0800

    Update ProductModal.jsx

--- frontend/src/components/ProductModal.jsx
+++ frontend/src/components/ProductModal.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import axios from "axios";
+import axios from from "../utils/axiosInstance";
 
 const ProductModal = ({ closeModal, saveProduct, editingProduct }) => {
   const [product, setProduct] = useState({
