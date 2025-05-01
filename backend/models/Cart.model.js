3dfecad923b5cb49779a997273893861ccbb3efd
Zeraijin301 <138296747+ZeroPinzaki301@users.noreply.github.com>
2025-05-01 21:43:10 +0800

    Update Cart.model.js
    Call to action implementations, if something goes wrong, rollback to the previous version

--- backend/models/Cart.model.js
+++ backend/models/Cart.model.js
@@ -25,6 +25,10 @@ const cartSchema = new mongoose.Schema(
         },
       },
     ],
+    isActive: {
+      type: Boolean,
+      default: true, // New carts are active by default
+    },
   },
   { timestamps: true } // Automatically adds createdAt and updatedAt
 );
